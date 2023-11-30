42b782e488a865a685c64b9dd47caf6ae0313c74
Tayomide <70061548+Tayomide@users.noreply.github.com>
2023-11-30 03:40:56 -0600

    Add default template container

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AppProps } from 'next/app'
+
+import '../styles/global.css'
+// import "tailwindcss/tailwind.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
